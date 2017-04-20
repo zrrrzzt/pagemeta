f1f0d4a3019357e6e0379960f433e8478a66ec0a
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-20 12:14:59 +0200

    Updates Node

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-'use strict';
+'use strict'
 
-var request = require('request');
-var validUrl = require('valid-url');
-var getMetadata = require('./lib/getMetadata');
+const request = require('request')
+const validUrl = require('valid-url')
+const getMetadata = require('./lib/getMetadata')
 
-function getPagemeta(uri, callback){
+module.exports = (uri, callback) => {
 
   if(!uri){
-    return callback(new Error('Missing required param'), null);
+    return callback(new Error('Missing required param'), null)
   }
 
-  if(!validUrl.isWebUri(uri)){
-    return callback(new Error('Invalid uri'), null);
+  if (!validUrl.isWebUri(uri)) {
+    return callback(new Error('Invalid uri'), null)
   }
 
-  request(uri, function(error, response, body){
-    if(error){
-      return callback(error, null);
+  request(uri, (error, response, body) => {
+    if (error) {
+      return callback(error, null)
     }
 
-    getMetadata(body.toString(), function(err, data){
+    getMetadata(body.toString(), function (err, data) {
       if(err){
-        return callback(err, null);
+        return callback(err, null)
       } else {
-        return callback(null, data);
+        return callback(null, data)
       }
-    });
-
-  });
-
+    })
+  })
 }
-
-module.exports = getPagemeta;
